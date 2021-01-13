1cc858b96eae1e28486657d1b784c863b132a60d
sachiniepa <sachi.epa96@gmail.com>
2021-01-13 09:03:01 +0530

    Adding endpoints

--- nodejs/src/server.js
+++ nodejs/src/server.js
@@ -1,13 +1,33 @@
-{
-    "name": "docker_web_app",
-    "version": "1.0.0",
-    "description": "Node.js on Docker",
-    "author": "First Last <first.last@example.com>",
-    "main": "server.js",
-    "scripts": {
-      "start": "node server.js"
-    },
-    "dependencies": {
-      "express": "^4.16.1"
-    }
-  }
+'use strict';
+
+const express = require('express');
+
+const PORT = 5000;
+const HOST = '0.0.0.0';
+
+const app = express();
+
+app.get('/add/', (req, res) => {
+  res.send('Adding to constellation microservice mock response \n');
+});
+
+app.get('/remove/', (req, res) => {
+    res.send('Removing from constellation microservice mock response \n');
+  });
+
+  app.get('/monitor/', (req, res) => {
+    res.send('Monitor satelites microservice mock response \n');
+  });
+
+  app.get('/track/', (req, res) => {
+    res.send('Track orbit microservice mock response \n');
+  });
+
+  app.get('/control/', (req, res) => {
+    res.send('Control orbit microservice mock response \n');
+  });
+
+
+app.listen(PORT, HOST);
+
+console.log(`Running version 3 on http://${HOST}:${PORT}`);
