3bfd0f33f91a30cfcca9e8d49ead1e12534b6fc2
sachiniepa <sachi.epa96@gmail.com>
2021-01-29 21:45:44 +0530

    Editing the mock response

--- nodejs/src/server.js
+++ nodejs/src/server.js
@@ -8,19 +8,19 @@ const HOST = '0.0.0.0';
 const app = express();
 
 app.get('/add/', (req, res) => {
-  res.status(200).send('Adding to constellation microservice mock response \n');
+  res.status(200).send('Adding to constellation microservice mock response. \n');
 });
 
 app.get('/remove/', (req, res) => {
-    res.status(200).send('Removing from constellation microservice mock response \n');
+    res.status(200).send('Removing from constellation microservice mock response. \n');
 });
 
 app.get('/monitor/', (req, res) => {
-    res.status(200).send('Monitor satelites microservice mock response \n');
+    res.status(200).send('Monitor satelites microservice mock response. \n');
 });
 
 app.get('/track/', (req, res) => {
-    res.status(200).send('Track orbit microservice mock response \n');
+    res.status(200).send('Track orbit microservice mock response. \n');
 });
 /*
 app.get('/control/', (req, res) => {
@@ -28,7 +28,7 @@ app.get('/control/', (req, res) => {
 });*/
 
 app.get('/control/', (req, res) => {
-    res.status(200).send('Control orbit microservice mock response \n');
+    res.status(200).send('Control orbit microservice mock response. \n');
 });
 
 app.listen(PORT, HOST);
